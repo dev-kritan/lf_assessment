2b6f34f6c8d91e4f74af40ba372894989f2e5b34
kritan <kritanshrestha560@gmail.com>
2026-08-28 22:15:23 +0545

    test: add isCreator property to mock event objects in BulkDeleteConfirmDialog tests

--- frontend/src/test/BulkDeleteConfirmDialog.test.tsx
+++ frontend/src/test/BulkDeleteConfirmDialog.test.tsx
@@ -21,6 +21,7 @@ const mockEvents: EventItem[] = [
     tags: [],
     rsvpStats: { yes: 10, maybe: 2, no: 1, total: 13 },
     userRsvp: 'yes',
+    isCreator: true,
   },
   {
     id: 2,
@@ -39,6 +40,7 @@ const mockEvents: EventItem[] = [
     tags: [],
     rsvpStats: { yes: 5, maybe: 1, no: 0, total: 6 },
     userRsvp: 'maybe',
+    isCreator: true,
   },
 ];
 
