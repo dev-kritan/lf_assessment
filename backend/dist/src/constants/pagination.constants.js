121ec3342cea93b7223afe1203063487a235fbd5
kritan <kritanshrestha560@gmail.com>
2026-08-31 09:49:12 +0545

    chore: remove dist folders from repository

--- backend/dist/src/constants/pagination.constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PAGINATION_DEFAULTS = void 0;
-exports.PAGINATION_DEFAULTS = {
-    PAGE: 1,
-    LIMIT: 6,
-    MAX_LIMIT: 100,
-    ATTENDEES_LIMIT: 50,
-};
